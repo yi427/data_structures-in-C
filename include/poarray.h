3628714eff0ea6095d68d6ed2cc4049aac1535df
zzr-rr <3210975872@qq.com>
2024-03-30 18:48:42 +0800

    initial dynamic array.

--- include/poarray.h
+++ include/poarray.h
@@ -9,14 +9,14 @@
 typedef struct __array_t {
     int size;
     int capacity;
-    Object *elementData[];
+    Object elementData[];
 } array_t;
 
 
-public void Array_Init(array_t *, int initialSize);
-private void Array_Ensure_Capacity(array_t *, int minCapacity);
-private void Array_Grow(array_t *, int minCapacity);
-private void Array_Add(array_t *, Object element);
+public array_t* Array_Init(int initialSize);
+private void Array_Ensure_Capacity(array_t **, int minCapacity);
+private void Array_Grow(array_t **, int minCapacity);
+private void Array_Add(array_t **, Object element);
 private void Array_Print(array_t *);
 
 #undef public
